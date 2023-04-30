fd20ca5ee4bc1be89816212f6db7449ba5b2a512
gIsForGravity <gavin.f.tantleff@gmail.com>
2023-04-29 22:48:41 -0400

    Initial commit

---
+++ src/main/java/co/tantleff/libanimate/Libanimate.java
@@ -0,0 +1,18 @@
+package co.tantleff.libanimate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class Libanimate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
