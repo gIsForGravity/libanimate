975c446cbba7a26d03f45c95941baec2b38f14fc
gIsForGravity <gavin.f.tantleff@gmail.com>
2023-04-29 23:38:12 -0400

    a little test command

--- src/main/java/co/tantleff/libanimate/Libanimate.java
+++ src/main/java/co/tantleff/libanimate/Libanimate.java
@@ -1,6 +1,13 @@
 package co.tantleff.libanimate;
 
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 @SuppressWarnings("unused")
 public final class Libanimate extends JavaPlugin {
@@ -8,7 +15,29 @@ public final class Libanimate extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Objects.requireNonNull(getCommand("testanimate")).setExecutor((commandSender, command, s, strings) -> {
+            if (!(commandSender instanceof final Player caller))
+                return false;
 
+            caller.sendMessage("doin the command or somethin");
+
+            caller.setGameMode(GameMode.SPECTATOR);
+
+            final var beginningLocation = caller.getLocation().add(0, 3, 0);
+            final var endLocation = beginningLocation.add(15, 0, 0);
+
+            assert caller.getLocation().getWorld() != null;
+
+            caller.getLocation().getWorld().spawn(beginningLocation, ItemDisplay.class, itemDisplay -> {
+                caller.setSpectatorTarget(itemDisplay);
+                itemDisplay.setItemStack(new ItemStack(Material.STICK));
+                itemDisplay.setInterpolationDelay(-1);
+                itemDisplay.setInterpolationDuration(100);
+                itemDisplay.teleport(endLocation);
+            });
+
+            return true;
+        });
     }
 
     @Override
